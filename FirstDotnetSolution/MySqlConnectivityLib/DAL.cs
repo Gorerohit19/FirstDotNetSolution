38ef35629a5c1df1d16e16bc25e938205f928ec7
unknown <gore.rohit1902@gmail.com>
2022-12-08 17:26:02 +0530

    MySql Crude Operations in Solution file

---
+++ FirstDotnetSolution/MySqlConnectivityLib/DAL.cs
@@ -0,0 +1,143 @@
+﻿using MySql.Data.MySqlClient;
+namespace MySqlConnectivityLib;
+public static class DAL
+{
+  public static string conStr = "server=localhost;uid=root;password=Rohit@7378;database=demo1";
+  
+  public static bool CreateTable(){
+    bool status = false;
+    MySqlConnection con = new MySqlConnection();
+    con.ConnectionString = conStr;
+
+    try{
+        string query = "create table mobiles(mobileId int not null primary key auto_increment,"+
+                        "mobileCompany varchar(20),"+
+                        "modelName varchar(20),"+
+                        "price decimal(10,2)"+
+                        ")";
+        Console.WriteLine(query);
+        con.Open();
+        MySqlCommand cmd = new MySqlCommand(query, con);
+        cmd.ExecuteNonQuery();
+        status = true;
+    }
+    catch(Exception e){
+        Console.WriteLine("Exception = "+e.Message);
+    }
+    finally{
+        con.Close();
+    }
+    return status;
+  }
+
+  public static bool Insert(string mobileCompany, string modelName, double price){
+    bool status = false;
+    MySqlConnection con = new MySqlConnection();
+    con.ConnectionString = conStr;
+    try{
+        string query = "insert into mobiles (mobileCompany, modelName, price) values ('"+mobileCompany+"','"+modelName+"','"+price+"')";
+        Console.WriteLine(query);
+        con.Open();
+        MySqlCommand cmd = new MySqlCommand(query, con);
+        cmd.ExecuteNonQuery();
+        status = true;
+    }
+    catch(Exception e){
+        Console.WriteLine("Exception = "+e.Message);
+    }
+    finally{
+        con.Close();
+    }
+    return status;
+  }
+
+  public static bool Update(int id, string mobileCompany,string modelName, double price){
+    bool status = false;
+    MySqlConnection con = new MySqlConnection();
+    con.ConnectionString = conStr;
+    try{
+        string query = "update mobiles set mobileCompany='"+mobileCompany+"',"+
+                            "modelName='"+modelName+"',"+
+                            "price="+price+
+                            " where mobileId="+id;
+        Console.WriteLine(query);
+        MySqlCommand cmd = new MySqlCommand(query, con);
+        con.Open();
+        cmd.ExecuteNonQuery();
+        status = true;
+    }
+    catch(Exception e){
+        Console.WriteLine("Exception = "+e.Message);
+    }
+    finally{
+        con.Close();
+    }
+    return status;
+  }
+   public static bool Delete(int id){
+    bool status = false;
+    MySqlConnection con = new MySqlConnection();
+    con.ConnectionString = conStr;
+    try{
+        string query = "Delete from mobiles where mobileId="+id;
+        Console.WriteLine(query);
+        MySqlCommand cmd = new MySqlCommand(query, con);
+        con.Open();
+        cmd.ExecuteNonQuery();
+        status=true;
+    }
+    catch(Exception e){
+        Console.WriteLine("Exception = "+e.Message);
+    }
+    finally{
+        con.Close();
+    }
+    return status;
+   }
+
+   public static void ShowAllMobiles(){
+    MySqlConnection con = new MySqlConnection();
+    con.ConnectionString = conStr;
+    try{
+        string query ="select * from mobiles";
+        Console.WriteLine(query);
+        MySqlCommand cmd = new MySqlCommand(query, con);
+        con.Open();
+        MySqlDataReader rdr = cmd.ExecuteReader();
+            while(rdr.Read()){
+                Console.WriteLine(rdr[0]+"--"+rdr[1]+"--"+rdr[2]+"--"+rdr[3]);
+            }
+            rdr.Close();
+        
+    }
+    catch(Exception e){
+        Console.WriteLine("Exception ="+e.Message);
+    }
+    finally{
+        con.Close();
+    }
+   }
+
+   public static void ShowMobileById(int id){
+        bool status = false;
+        MySqlConnection con = new MySqlConnection();
+        con.ConnectionString = conStr;
+        try{
+             con.Open();
+            string query = "select * from mobiles where mobileId="+id;
+            MySqlCommand cmd = new MySqlCommand(query,con);
+            MySqlDataReader rdr = cmd.ExecuteReader();
+            if(rdr.Read()){
+               Console.WriteLine(rdr[0]+"--"+rdr[1]+"--"+rdr[2]+"--"+rdr[3]); 
+               status = true;
+            }
+            rdr.Close();
+        }
+        catch(Exception e){
+            Console.WriteLine("Exception = "+e.Message);
+        }
+        finally{
+            con.Close();
+        }
+   }
+}
--- FirstDotnetSolution/TestApp/Program.cs
+++ FirstDotnetSolution/TestApp/Program.cs
@@ -1,6 +1,7 @@
 ﻿using CatalogLib;
 using OrderProcessingLib;
 using CRMLib;
+using MySqlConnectivityLib;
 
 Product p1 = new Product();
 p1.Id=1;
@@ -29,3 +30,26 @@ Console.WriteLine(p1.Id+" "+p1.Title+" "+p1.Discription+" "+p1.UnitPrice+" "+p1.
 Console.WriteLine(o1.OrderDate+" "+o1.CustomerName+" "+o1.TotalAmount+" "+o1.Status);
 Console.WriteLine(c1.Id+" "+c1.FirstName+" "+c1.LastName+" "+c1.ContactNumber+" "+c1.Email);
 
+/*
+//Creating Table 
+//DAL.CreateTable();
+//Insrting Info in Table
+DAL.Insert("Apple","Iphone 14",129000);
+DAL.Insert("Samsung","S22 Ultra",100000);
+DAL.Insert("Vivo","X32",89000);
+*/
+
+/*
+//Showing All Mobiles in Table
+DAL.ShowAllMobiles();
+*/
+
+/*
+//Updating info in id = 3
+DAL.Update(3,"Moto G","Edge",20000);
+*/
+//Deleting row from Table
+//DAL.Delete(3);
+
+//Showing Mobile by its id
+DAL.ShowMobileById(1);
